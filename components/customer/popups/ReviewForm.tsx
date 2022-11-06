27b940c5133666fab367269c32d2c575cc79f48c
Piotr Gerke <piotrg2003@gmail.com>
2022-11-06 10:49:40 +0100

    version conflict resolved

--- components/customer/popups/ReviewForm.tsx
+++ components/customer/popups/ReviewForm.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { useState, useEffect } from 'react';
-import StarRatingComponent from 'react-star-rating-component';
+// import StarRatingComponent from 'react-star-rating-component';
 
 const ReviewForm = ({ onCloseModal, setOpenReview, setOpenReviewSuccess }: any) => {
 
@@ -38,7 +38,7 @@ const ReviewForm = ({ onCloseModal, setOpenReview, setOpenReviewSuccess }: any) 
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full h-screen flex justify-center items-center backdrop-blur-lg cursor-pointer'>
-      <div className="sm:mx-auto w-11/12 sm:w-full sm:max-w-md rounded-xl shadow-[2px_2px_50px_rgba(0,0,0,0.25)]">
+      {/* <div className="sm:mx-auto w-11/12 sm:w-full sm:max-w-md rounded-xl shadow-[2px_2px_50px_rgba(0,0,0,0.25)]">
         <div ref={ref} className="bg-white py-16 px-8 shadow sm:rounded-lg sm:px-10 border-2 rounded-xl">
           <div className="flex min-h-full flex-col justify-center sm:px-6 lg:px-8 ">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -79,7 +79,7 @@ const ReviewForm = ({ onCloseModal, setOpenReview, setOpenReviewSuccess }: any) 
             </div>
           </form>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 };
